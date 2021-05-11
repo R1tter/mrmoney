54b281af30498730d7e035a0577cb0bb2c6b6d9c
Marcelo Ritter <marcelo.ritter@dotgroup.com.br>
2021-05-11 15:00:33 -0300

    Config: Global CSS and color pallete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global"
+
 export function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
